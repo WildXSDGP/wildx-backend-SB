483b70ebf8cf74a573e6acce16d88e4904af3d1b
Banuka Malshan <bmagamulla@gmail.com>
2026-03-20 12:11:51 +0530

    Constructor inject Animal Marker Repository

--- src/main/java/com/sdgp/backend/wildx/service/MarkerService.java
+++ src/main/java/com/sdgp/backend/wildx/service/MarkerService.java
@@ -1,5 +1,17 @@
 package com.sdgp.backend.wildx.service;
 
+import org.springframework.stereotype.Service;
+
+import com.sdgp.backend.wildx.repository.AnimalMarkerRepository;
+
+@Service
 public class MarkerService {
+	
+	private final AnimalMarkerRepository markerRepository;
+	
+	//constructor injection
+	public MarkerService(AnimalMarkerRepository markerRepository) {
+		this.markerRepository= markerRepository;
+	}
 
 }
