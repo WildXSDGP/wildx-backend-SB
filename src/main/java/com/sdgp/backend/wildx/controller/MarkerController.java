05be440c130e714767d17e9a75526b781c38facf
Banuka Malshan <bmagamulla@gmail.com>
2026-03-20 12:37:06 +0530

    Implement controller endpoint  get all  markers .

--- src/main/java/com/sdgp/backend/wildx/controller/MarkerController.java
+++ src/main/java/com/sdgp/backend/wildx/controller/MarkerController.java
@@ -1,8 +1,14 @@
 package com.sdgp.backend.wildx.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sdgp.backend.wildx.model.AnimalMarker;
 import com.sdgp.backend.wildx.service.MarkerService;
 
 @RestController
@@ -16,6 +22,11 @@ public class MarkerController {
 		this.markerService=markerService;
 	}
 	
-	
+	//Get All parks 
+	@GetMapping("/park/{parkId}")
+    public ResponseEntity<List<AnimalMarker>> getMarkersByPark(@PathVariable Long parkId) {
+        List<AnimalMarker> markers = markerService.getMarkersByPark(parkId);
+        return ResponseEntity.ok(markers);
+    }
 
 }
